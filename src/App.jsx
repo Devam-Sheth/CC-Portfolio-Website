d38e4b02e36548061732713d5b501de767762f8d
sachinagr446 <145377162+sachinagr446@users.noreply.github.com>
2024-08-04 01:18:17 +0530

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 
-
+import './App.css'
 import Header from './components/Header/Header'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    
+    <Header/>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../assets/images/Logo.png";
+import { Typewriter } from 'react-simple-typewriter'
 function Header() {
   return (
     <div className="header">
       <div className="header-content flex">
         <div className="logo">
-          <img src={logo} className="logo" alt="" />
+          <img src={logo} className="logo-img" alt="" />
           <div className="center">
-            <h1 className="Mothercode">CODING CLUB</h1>
-            <h2>BITS PILANI</h2>
-            
+            <h1 className="Mothercode">
+            <Typewriter words={["CODING CLUB"]} loop={false} cursor typeSpeed={50} deleteSpeed={50} delaySpeed={2000}  cursorStyle='|'/>
+                </h1>
+            <h2  className="poppins" style={{margin:0}}>BITS PILANI</h2>
           </div>
         </div>
-       
+        <div className="center">
+          <h3 className="anta">
+            We're the merc with a mouth, but with keyboards instead of katanas
+          </h3>
+        </div>
       </div>
-      
+
       {/* <div className='center'>
         <h3>We're the merc with a mouth, but with keyboards instead of katanas</h3>
         </div> */}
-      </div>
-    
+    </div>
   );
 }
 
