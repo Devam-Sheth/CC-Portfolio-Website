8d7b2705877227fd809ea59de2df85bcab0194b0
paritjain <144093967+paritjain@users.noreply.github.com>
2024-08-06 10:40:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Carousel from "./components/Carousel/Carousel";
+import ContactUs from "./components/ContactUs/ContactUs";
 function App() {
   return (
     <>
       <Carousel />
+      <ContactUs />
     </>
   );
 }
