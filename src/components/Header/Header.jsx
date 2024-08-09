4b4e1a9cef6d7227b38643a4c5a851d3b679244c
sachinagr446 <145377162+sachinagr446@users.noreply.github.com>
2024-08-10 00:37:35 +0530

    change quote

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./Header.css";
+import { useState,useEffect } from "react";
 import logo from "../../assets/images/Logo.png";
 import { Typewriter } from 'react-simple-typewriter'
 function Header() {
+const [visible, setVisible] = useState(false);
+  const handleDone=()=>{
+      setVisible(true)
+  }
+  // useEffect(() => {
+    
+  
+  
+  // }, [])
+ 
+    const [showHeading, setShowHeading] = useState(false);
+    const [showQuote, setShowQuote] = useState(false);
+
+    useEffect(() => {
+        const headTimer = setTimeout(() => {
+            setShowHeading(true);
+        }, 1300); //
+        const quoteTimer=setTimeout(()=>{
+          setShowQuote(true)
+        },  [2600]);
+
+        
+        return () =>
+          {clearTimeout(headTimer);
+            clearTimeout(quoteTimer);
+        } 
+    }, []);
   return (
     <div className="header">
       <div className="header-content flex">
@@ -10,14 +38,20 @@ function Header() {
           <img src={logo} className="logo-img" alt="" />
           <div className="center">
             <h1 className="Mothercode">
-            <Typewriter words={["CODING CLUB"]} loop={false} cursor typeSpeed={100} deleteSpeed={50} delaySpeed={2000}  cursorStyle='|'/>
+            <Typewriter words={["CODING CLUB"]} loop={1}  typeSpeed={100} deleteSpeed={50} delaySpeed={2000}  cursorStyle='|' onLoopDone={handleDone}/>
                 </h1>
-            <h2  className="poppins" style={{margin:0}}>BITS PILANI</h2>
+        {   showHeading &&
+            <h2  className="poppins" style={{margin:0}}> <Typewriter words={["BITS PILANI"]} loop={1}  typeSpeed={100} deleteSpeed={50} delaySpeed={2000}  cursorStyle='|'  /></h2>
+           }
+           
           </div>
         </div>
         <div className="center">
           <h3 className="anta">
-            We're the merc with a mouth, but with keyboards instead of katanas
+            { showQuote && 
+              <Typewriter words={["We code in the name of the Old Gods and the New","We are the Coding Club, we can do whatever the f**k we want"]} loop={0} cursor typeSpeed={50} deleteSpeed={40} delaySpeed={2000}  cursorStyle='|'  />}
+         
+           
           </h3>
         </div>
       </div>
