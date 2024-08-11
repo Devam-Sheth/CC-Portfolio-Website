e236d61cbc4328065061d21cff0f96d42282f625
Devam-Sheth <devamsheth1203@gmail.com>
2024-08-11 12:39:09 +0530

    added header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Header.css";
-import { useState,useEffect } from "react";
 import logo from "../../assets/images/Logo.png";
 import { Typewriter } from "react-simple-typewriter";
 import Tilt from "./Tilt";
 import { Howl } from 'react-howler';
 
-<<<<<<< HEAD
 function Header() {
-=======
-const [visible, setVisible] = useState(false);
-  const handleDone=()=>{
-      setVisible(true)
-  }
- 
-    const [showHeading, setShowHeading] = useState(false);
-    const [showQuote, setShowQuote] = useState(false);
-
-    useEffect(() => {
-        const headTimer = setTimeout(() => {
-            setShowHeading(true);
-        }, 1300); //
-        const quoteTimer=setTimeout(()=>{
-          setShowQuote(true)
-        },  [2600]);
-
-        
-        return () =>
-          {clearTimeout(headTimer);
-            clearTimeout(quoteTimer);
-        } 
-    }, []);
-
->>>>>>> 918182869f46fd0add8a7acab61258f782d6b9ae
   const tiltEffectSettings = {
     max: 25,
     perspective: 1000,
@@ -52,7 +25,6 @@ const [visible, setVisible] = useState(false);
           </Tilt>
           <div className="center">
             <h1 className="Mothercode">
-<<<<<<< HEAD
               <Typewriter
                 words={words}
                 loop={false}
@@ -67,23 +39,7 @@ const [visible, setVisible] = useState(false);
             <h2 className="poppins" style={{ margin: 0 }}>
               BITS PILANI
             </h2>
-=======
-            <Typewriter words={["CODING CLUB"]} loop={1}  typeSpeed={100} deleteSpeed={50} delaySpeed={2000}  cursorStyle='_' onLoopDone={handleDone}/>
-                </h1>
-        {   showHeading &&
-            <h2  className="poppins" style={{margin:0}}> <Typewriter words={["BITS PILANI"]} loop={1}  typeSpeed={100} deleteSpeed={50} delaySpeed={2000}  cursorStyle='|'  /></h2>
-           }
-           
->>>>>>> 918182869f46fd0add8a7acab61258f782d6b9ae
           </div>
-        </div>
-        <div className="center">
-          <h3 className="anta">
-           { showQuote && 
-              <Typewriter words={["We code in the name of the Old Gods and the New","We are the Coding Club, we can do whatever the f**k we want"]} loop={0} cursor typeSpeed={50} deleteSpeed={40} delaySpeed={3000}  cursorStyle='|'  />}
-         
-           
-          </h3>
         </div>
       </div>
     </div>
